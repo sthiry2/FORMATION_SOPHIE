0e46f6d2a8e9ac6f146cb7a1ee6eef089f7856a4
sthiry2 <sthiry@arfp.asso.fr>
2021-09-28 09:48:10 +0200

    ST Ajout Evenement Class Metier

---
+++ Cours Objet/2.ExempleEvenementSurClasseMetier/ClassLibraryPersonne/Personne.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPersonne
+{
+    public class Personne
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; /*set => name = value;*/ }
+        public int Age { get => age; /*set => age = value;*/ }
+
+        public delegate void DelegateMajeur(Personne sender);
+        public event DelegateMajeur JeViensDEtreMajeur;
+
+        //rajouter un evenement RisqueCrise...
+
+        public Personne(string name)
+        {
+            this.name = name;
+            age = 0;
+        }
+
+        public void VieillirDUnAn()
+        {
+            age++;
+            if (age == 18)
+            { 
+                if (JeViensDEtreMajeur!=null) //verification que qq s'est enregistre
+                {
+                    JeViensDEtreMajeur(this);
+                }
+            }
+        }
+    }
+}
---
+++ Cours Objet/2.ExempleEvenementSurClasseMetier/ConsoleAppTestEvenementMajorite/Program.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibraryPersonne;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTestEvenementMajorite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p = new Personne("Robert");
+            Personne p2 = new Personne("Berthe");
+
+            p.JeViensDEtreMajeur += P_JeViensDEtreMajeur;
+            p2.JeViensDEtreMajeur += P2_JeViensDEtreMajeur;
+
+            for (int i = 0; i < 100; i++)
+            {
+                p.VieillirDUnAn();
+                p2.VieillirDUnAn();
+            }            
+        }
+
+        private static void P2_JeViensDEtreMajeur(Personne sender)
+        {
+            Console.WriteLine("Bravo " + ((Personne)sender).Name);
+        }
+
+        private static void P_JeViensDEtreMajeur(Personne sender)
+        {
+            // sender.Age--;
+            Console.WriteLine("Ca y est "+ sender.Name + " est majeur! Il peut se debrouiller par lui même");
+        }
+    }
+}
